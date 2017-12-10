180983d551b05b861f20b78ced80d77ea11117e9
Arsenalist <root@raptorsrepublic.com>
2017-12-10 13:10:12 -0500

    Update based on Instagram changes

--- gen.py
+++ gen.py
@@ -15,21 +15,24 @@ def create_file(file_name, file_content):
     f.write(file_content)
     f.close()
 
-users = ["pdpatt", "demar_derozan", "kyle_lowry7", "jvalanciunas", "3tross1", "lucasbebenogueira", "brunofive", "demarrecarroll1", "coryjoseph", "dwfivefive", "normanpowell4", "pskills43", "bismackbiyombo", "iamjames_johnson3"]
+
+
+users = ["oanunoby", "officialzobrown", "brunofive", "demar_derozan", "sergeibaka7", "kyle_lowry7", "_alvo_", "masfresco", "bebe92official", "jakob", "normanpowell4", "pskills43", "jvalanciunas", "fredvanvleet", "delonwright", "raptors", "raptors905"]
 
 all_media = []
 for u in users:
     try:
       soup = BeautifulSoup(requests.get("http://www.instagram.com/" + u).text, "html.parser")
       scripts = soup.find_all("script")
-      data = json.loads(scripts[6].get_text()[:-1].split("window._sharedData = ")[1])
-      recent_media = data["entry_data"]["ProfilePage"][0]["user"]["media"]["nodes"]
-      for r in recent_media:
-          r["user"] = data["entry_data"]["ProfilePage"][0]["user"]
-          r['time'] = time.strftime('%b %d, %Y', time.localtime(r['date']))
-      all_media.extend(recent_media)
+      for script in scripts:
+          if "window._sharedData" in script.string:
+              data = json.loads(script.get_text()[:-1].split("window._sharedData = ")[1])
+              recent_media = data["entry_data"]["ProfilePage"][0]["user"]["media"]["nodes"]
+              for r in recent_media:
+                  r["user"] = data["entry_data"]["ProfilePage"][0]["user"]
+                  r['time'] = time.strftime('%b %d, %Y', time.localtime(r['date']))
+              all_media.extend(recent_media)
     except Exception, e:
-      print e
       continue
 sorted_media = sorted(all_media, key=itemgetter('date'), reverse=True)
 sorted_media = sorted_media[:200]
